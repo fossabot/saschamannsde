0451e37ec5214bc119147a7df170c45ca831a9b4
Sascha Manns <Sascha.Manns@outlook.de>
2022-12-16 17:18:18 +0100

    Cleanup. LW

--- src/MannsBlog/wwwroot/3rdparty/js/syntaxhighlight.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-  // Add default coloring for old scripts
-  $("code:not([class])").addClass("brush: csharp");
-  $("pre:not([class])").addClass("brush: csharp");
-  SyntaxHighlighter.defaults.tabSize = 2;
-  SyntaxHighlighter.defaults.fontSize = ".8em";
-  var blocks = $("pre > code");
-  for (var x = 0; x < blocks.length; ++x) SyntaxHighlighter.highlight(SyntaxHighlighter.defaults, blocks[x]);
-});
