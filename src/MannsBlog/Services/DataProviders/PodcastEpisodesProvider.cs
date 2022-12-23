21a7c72ed962f849fd4e0ee69f3c410707ad665d
Sascha Manns <Sascha.Manns@outlook.de>
2022-12-23 12:59:40 +0100

    Moved License from GPL to MIT to pacify Syncfusions rules

--- src/MannsBlog/Services/DataProviders/PodcastEpisodesProvider.cs
+++ src/MannsBlog/Services/DataProviders/PodcastEpisodesProvider.cs
@@ -1,66 +1,73 @@
-﻿// Copyright (C) 2021 Sascha Manns <Sascha.Manns@outlook.de>
+﻿// MIT License
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// Copyright (c) 2022 Sascha Manns
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
 namespace MannsBlog.Services.DataProviders
 {
-  public class PodcastEpisodesProvider : DataProvider<PodcastEpisode>
-  {
-    public PodcastEpisodesProvider(IHostEnvironment env)
-      : base(env, "episodeList.json")
+    public class PodcastEpisodesProvider : DataProvider<PodcastEpisode>
     {
+        public PodcastEpisodesProvider(IHostEnvironment env)
+          : base(env, "episodeList.json")
+        {
+        }
+
+        public override IEnumerable<PodcastEpisode> Get()
+        {
+            return base.Get().OrderByDescending(a => a.PublishedDate).ToList();
+        }
     }
 
-    public override IEnumerable<PodcastEpisode> Get()
+    public class PodcastEpisode
     {
-      return base.Get().OrderByDescending(a => a.PublishedDate).ToList();
+        public string[] Blurb { get; set; } = new string[0];
+        public int EpisodeNumber { get; set; }
+        public string[] GuestBio { get; set; } = new string[0];
+        public string GuestName { get; set; } = "";
+        public string GuestHeadshot { get; set; } = "";
+        public string GuestFirstMachineImage { get; set; } = "";
+        public string GuestFirstMachineLink { get; set; } = "";
+        public string GuestFirstMachineName { get; set; } = "";
+        public ICollection<GuestLink> GuestLinks { get; set; } = new List<GuestLink>();
+        public string PodcastName { get; set; } = "";
+        public string AudioLink { get; set; } = "";
+        public DateTime PublishedDate { get; set; }
+        public string YouTubeLink { get; set; } = "";
+        public PodcastEpisodeStatus Status { get; set; }
     }
-  }
 
-  public class PodcastEpisode
-  {
-    public string[] Blurb { get; set; } = new string[0];
-    public int EpisodeNumber { get; set; }
-    public string[] GuestBio { get; set; } = new string[0];
-    public string GuestName { get; set; } = "";
-    public string GuestHeadshot { get; set; } = "";
-    public string GuestFirstMachineImage { get; set; } = "";
-    public string GuestFirstMachineLink { get; set; } = "";
-    public string GuestFirstMachineName { get; set; } = "";
-    public ICollection<GuestLink> GuestLinks { get; set; } = new List<GuestLink>();
-    public string PodcastName { get; set; } = "";
-    public string AudioLink { get; set; } = "";
-    public DateTime PublishedDate { get; set; }
-    public string YouTubeLink { get; set; } = "";
-    public PodcastEpisodeStatus Status { get; set; }
-  }
+    public enum PodcastEpisodeStatus
+    {
+        Planned,
+        Live,
+        Staged
+    }
 
-  public enum PodcastEpisodeStatus
-  {
-    Planned,
-    Live,
-    Staged
-  }
-
-  public class GuestLink
-  {
-    public string Name { get; set; } = "";
-    public string Link { get; set; } = "";
-  }
+    public class GuestLink
+    {
+        public string Name { get; set; } = "";
+        public string Link { get; set; } = "";
+    }
 }
